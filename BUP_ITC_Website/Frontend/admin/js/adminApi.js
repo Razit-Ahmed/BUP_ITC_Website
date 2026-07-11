b7c063807a0fbb82fc4cd9c51a27472c8a8c8772
razit ahmed <razit.ahmed05.com>
2026-07-11 19:02:10 +0600

    api js update

--- BUP_ITC_Website/Frontend/admin/js/adminApi.js
+++ BUP_ITC_Website/Frontend/admin/js/adminApi.js
@@ -2,7 +2,7 @@ export const API_BASE =
   (window.location.hostname === "localhost" ||
    window.location.hostname === "127.0.0.1")
     ? "http://localhost:3000"
-    : window.location.origin;
+    : "https://bup-itc.onrender.com";
 
 function getAuthToken() {
   return localStorage.getItem("adminToken");
